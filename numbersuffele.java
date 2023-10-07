0466b7df6cb4232c0614e98e0f353057d9240a07
khursidk <83726362+khursidk@users.noreply.github.com>
2023-10-07 11:08:05 +0530

    Update numbersuffele.java

--- numbersuffele.java
+++ numbersuffele.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+        List<Integer> myList = new ArrayList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(6);
+        myList.add(7);
+
+        Collections.shuffle(myList);
+
+        Integer[] shuffledArray = myList.toArray(new Integer[0]);
+
+        for (int value : shuffledArray) {
+            System.out.print(value + " ");
+        }
+    }
+}
 
